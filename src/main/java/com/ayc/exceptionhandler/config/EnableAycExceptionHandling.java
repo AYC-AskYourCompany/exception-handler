901bd526b8a444e52dbc0a03e8868ba7a343ef26
Sautino <marcelsauter9299@gmail.com>
2021-01-18 18:07:15 +0100

    Remove unnecessary code

--- src/main/java/com/ayc/exceptionhandler/config/EnableAycExceptionHandling.java
+++ src/main/java/com/ayc/exceptionhandler/config/EnableAycExceptionHandling.java
@@ -1,7 +1,8 @@
 package com.ayc.exceptionhandler.config;
 
+import com.ayc.exceptionhandler.exception.EntityNotFound;
 import org.springframework.context.annotation.Import;
 
-@Import({})
+@Import({ EntityNotFound.class })
 public @interface EnableAycExceptionHandling {
 }
